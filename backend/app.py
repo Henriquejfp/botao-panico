fb64f4526c8d0b7ef9a64268d9c559ba6f29aed1
Henrique <lu.henrigomea@gmail.com>
2026-05-27 16:46:32 -0300

    Agora vai

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,14 @@ import os
 
 app = Flask(__name__)
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+from supabase import create_client, Client
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 @app.route("/")
 def home():
